b83e9551dd36b9639dca802ca062c898d6e1c933
Pratham <pratham247@cqst.in>
2026-07-20 12:09:47 +0530

    frontier.cpp : implemented front(),dequeue(), isEmpty(),Size() and clear() method

--- src/frontier/frontier.cpp
+++ src/frontier/frontier.cpp
@@ -13,3 +13,29 @@ void Frontier::enqueue(const std::string& url, int depth){
     frontier.insertBack(FrontierItem(url, depth));
 }
 
+FrontierItem Frontier::dequeue(){
+    if (frontier.isEmpty()){
+        throw std::underflow_error("Frontier is empty");
+    }
+
+    FrontierItem item = frontier.front();
+    frontier.deleteFront();
+    return item;
+}
+
+const FrontierItem& Frontier::front() const{
+    if (frontier.isEmpty()){
+        throw std::underflow_error("Frontier is empty");
+    }
+    return frontier.front();
+}
+
+bool Frontier::isEmpty() const{
+    return frontier.isEmpty();
+}
+int Frontier::size() const{
+    return frontier.size();
+}
+void Frontier::clear(){
+    frontier.clear();
+}
